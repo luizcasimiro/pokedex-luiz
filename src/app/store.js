41b50278cff682ba7af31cc45925ec4fba4758e8
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-28 16:27:06 -0300

    add searchReducer to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listReducer from "../features/list/listSlice";
+import searchReducer from '../features/search/searchSlice'
 
 const store = configureStore({
   reducer: {
-    list: listReducer
+    list: listReducer,
+    search: searchReducer
   }
 });
 
