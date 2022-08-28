e30d2589476d6bffe58bb0029ef13040f92ffc8c
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-28 16:02:12 -0300

    create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+
+      <Header />
+
+      <h1>Welcome to your Pokédex!</h1>
+
+    </div>
+  );
+}
+
+export default Home;
