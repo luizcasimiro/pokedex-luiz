c921d35f7f1daf077f575fe5d22d4c7a345b798a
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-28 16:04:35 -0300

    add route to Home.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import Home from '../pages/Home';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/home" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
